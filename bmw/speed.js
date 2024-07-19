e88136d4903a3dd0ff4b04d2ecebf8f469bd8702
ibraah tech <ezramatu9@gmail.com>
2024-07-19 22:03:39 +0300

    Update speed.js

--- bmw/speed.js
+++ bmw/speed.js
@@ -22,17 +22,17 @@ const img = 'https://telegra.ph/file/17c83719a1b40e02971e4.jpg';
 const releaseDate = new Date(data.created_at).toLocaleDateString('en-GB');
             const lastUpdateDate = new Date(data.updated_at).toLocaleDateString('en-GB');
 
-const gitdata = `𝐁𝐌𝐖 𝐌𝐃 𝐆𝐈𝐓𝐇𝐔𝐁 𝐈𝐍𝐅𝐎 \n𝐂𝐑𝐄𝐓𝐄𝐃 𝐁𝐘 𝐈𝐁𝐑𝐀𝐇𝐈𝐌 𝐀𝐃𝐀𝐌𝐒
+const gitdata = `𝐁𝐌𝐖 𝐌𝐃 𝐆𝐈𝐓𝐇𝐔𝐁 𝐈𝐍𝐅𝐎𝐌𝐄𝐓𝐈𝐎𝐍.  \n𝐂𝐑𝐄𝐓𝐄𝐃 𝐁𝐘 𝐈𝐁𝐑𝐀𝐇𝐈𝐌 𝐀𝐃𝐀𝐌𝐒.
 
-𝐒𝐓𝐀𝐑⭐ 𝐓𝐇𝐄 𝐑𝐄𝐏𝐎 𝐓𝐇𝐄𝐍 𝐅𝐎𝐑𝐊🍴
+𝐒𝐓𝐀𝐑 ⭐ 𝐓𝐇𝐄 𝐑𝐄𝐏𝐎 𝐓𝐇𝐄𝐍 𝐅𝐎𝐑𝐊 🍴
 
-📂 Repository Name: ${repoInfo.name}
-📝 Description: ${repoInfo.description}
-👤 Owner: ${repoInfo.owner.login}
+📂 Repository Name: *BMW-MD*
+📝 Last Update: ${repoInfo.lastUpdate}
+👤 Owner: *Ibrahim Adams*
 ⭐ Stars: ${repoInfo.stars}
 🍴 Forks: ${repoInfo.forks}
-🌐 URL: ${repoInfo.html_url}
-📲YouTube : _https://www.youtube.com/@ibrahimaotech_
+🌐 Repo: ${data.html_url}
+⭕ For More Info : https://github.com/IBRAHIM-TECH-AI/IBRAHIM-ADAMS-INFO⁠
 `;
 
 
@@ -45,4 +45,139 @@ console.log("Could not fetch data")
 
 
 });
+
+zokou({ nomCom: "botrep", reaction: "📂", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+
+
+const githubRepo = 'https://api.github.com/repos/devibraah/BWM-XMD';
+const img = 'https://telegra.ph/file/17c83719a1b40e02971e4.jpg';
+
+
+    const response = await fetch(githubRepo); 
+        const data = await response.json(); 
+ 
+        if (data) {
+            const repoInfo = {
+                stars: data.stargazers_count,
+                forks: data.forks_count,
+                lastUpdate: data.updated_at,
+                owner: data.owner.login,
+               
+            };
+const releaseDate = new Date(data.created_at).toLocaleDateString('en-GB');
+            const lastUpdateDate = new Date(data.updated_at).toLocaleDateString('en-GB');
+
+const gitdata = `𝐁𝐌𝐖 𝐌𝐃 𝐆𝐈𝐓𝐇𝐔𝐁 𝐈𝐍𝐅𝐎𝐌𝐄𝐓𝐈𝐎𝐍.  \n𝐂𝐑𝐄𝐓𝐄𝐃 𝐁𝐘 𝐈𝐁𝐑𝐀𝐇𝐈𝐌 𝐀𝐃𝐀𝐌𝐒.
+
+𝐒𝐓𝐀𝐑 ⭐ 𝐓𝐇𝐄 𝐑𝐄𝐏𝐎 𝐓𝐇𝐄𝐍 𝐅𝐎𝐑𝐊 🍴
+
+📂 Repository Name: *BMW-MD*
+📝 Last Update: ${repoInfo.lastUpdate}
+👤 Owner: *Ibrahim Adams*
+⭐ Stars: ${repoInfo.stars}
+🍴 Forks: ${repoInfo.forks}
+🌐 Repo: ${data.html_url}
+⭕ For More Info : https://github.com/IBRAHIM-TECH-AI/IBRAHIM-ADAMS`;
+
+
+await zk.sendMessage(dest, { image: { url: img }, caption: gitdata });
+
+} else {
+console.log("Could not fetch data")
+
+}
+
+
+});
+
+zokou({ nomCom: "botrepo1", reaction: "📂", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+
+
+const githubRepo = 'https://api.github.com/repos/devibraah/BWM-XMD';
+const img = 'https://telegra.ph/file/17c83719a1b40e02971e4.jpg';
+
+
+    const response = await fetch(githubRepo); 
+        const data = await response.json(); 
+ 
+        if (data) {
+            const repoInfo = {
+                stars: data.stargazers_count,
+                forks: data.forks_count,
+                lastUpdate: data.updated_at,
+                owner: data.owner.login,
+               
+            };
+const releaseDate = new Date(data.created_at).toLocaleDateString('en-GB');
+            const lastUpdateDate = new Date(data.updated_at).toLocaleDateString('en-GB');
+
+const gitdata = `𝐁𝐌𝐖 𝐌𝐃 𝐆𝐈𝐓𝐇𝐔𝐁 𝐈𝐍𝐅𝐎𝐌𝐄𝐓𝐈𝐎𝐍.  \n𝐂𝐑𝐄𝐓𝐄𝐃 𝐁𝐘 𝐈𝐁𝐑𝐀𝐇𝐈𝐌 𝐀𝐃𝐀𝐌𝐒.
+
+𝐒𝐓𝐀𝐑 ⭐ 𝐓𝐇𝐄 𝐑𝐄𝐏𝐎 𝐓𝐇𝐄𝐍 𝐅𝐎𝐑𝐊 🍴
+
+📂 Repository Name: *BMW-MD*
+📝 Last Update: ${repoInfo.lastUpdate}
+👤 Owner: *Ibrahim Adams*
+⭐ Stars: ${repoInfo.stars}
+🍴 Forks: ${repoInfo.forks}
+🌐 Repo: ${data.html_url}
+⭕ For More Info : https://github.com/IBRAHIM-TECH-AI/IBRAHIM-ADAMS-INFO⁠
+`;
+
+
+await zk.sendMessage(dest, { image: { url: img }, caption: gitdata });
+
+} else {
+console.log("Could not fetch data")
+
+}
+
+
+});
+
+zokou({ nomCom: "botrepo2", reaction: "📂", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+
+
+const githubRepo = 'https://api.github.com/repos/devibraah/BWM-XMD';
+const img = 'https://telegra.ph/file/17c83719a1b40e02971e4.jpg';
+
+
+    const response = await fetch(githubRepo); 
+        const data = await response.json(); 
+ 
+        if (data) {
+            const repoInfo = {
+                stars: data.stargazers_count,
+                forks: data.forks_count,
+                lastUpdate: data.updated_at,
+                owner: data.owner.login,
+               
+            };
+const releaseDate = new Date(data.created_at).toLocaleDateString('en-GB');
+            const lastUpdateDate = new Date(data.updated_at).toLocaleDateString('en-GB');
+
+const gitdata = `𝐁𝐌𝐖 𝐌𝐃 𝐆𝐈𝐓𝐇𝐔𝐁 𝐈𝐍𝐅𝐎𝐌𝐄𝐓𝐈𝐎𝐍.  \n𝐂𝐑𝐄𝐓𝐄𝐃 𝐁𝐘 𝐈𝐁𝐑𝐀𝐇𝐈𝐌 𝐀𝐃𝐀𝐌𝐒.
+
+𝐒𝐓𝐀𝐑 ⭐ 𝐓𝐇𝐄 𝐑𝐄𝐏𝐎 𝐓𝐇𝐄𝐍 𝐅𝐎𝐑𝐊 🍴
+
+📂 Repository Name: *BMW-MD*
+📝 Last Update: ${repoInfo.lastUpdate}
+👤 Owner: *Ibrahim Adams*
+⭐ Stars: ${repoInfo.stars}
+🍴 Forks: ${repoInfo.forks}
+🌐 Repo: ${data.html_url}
+⭕ For More Info : https://github.com/IBRAHIM-TECH-AI/IBRAHIM-ADAMS-INFO⁠
+`;
+
+
+await zk.sendMessage(dest, { image: { url: img }, caption: gitdata });
+
+} else {
+console.log("Could not fetch data")
+
+}
+
+
+});
+
 
