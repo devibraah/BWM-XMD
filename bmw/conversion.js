5d7e507dcf273d7cf8d721a312b28581f50e9e81
ibraah tech <ezramatu9@gmail.com>
2024-07-19 19:53:24 +0300

    mega💲

--- bmw/conversion.js
+++ bmw/conversion.js
@@ -70,7 +70,7 @@ const alea = (ext) => {
     }
 
     sticker = new Sticker(buffer, {
-      pack:"Beltah-Md" ,
+      pack:"BMW-Md" ,
       author: nomAuteurMessage,
       type:
         arg.includes("crop") || arg.includes("c")
