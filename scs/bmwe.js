e5cbf22076aca63eec6191cb98118501dc9d5618
ekitale <munjueko@gmail.com>
2024-08-29 16:42:54 +0300

    ⭕ ADAMS VERSION ⭕

--- scs/bmwe.js
+++ scs/bmwe.js
@@ -2,7 +2,7 @@ const { adams } = require("../Ibrahim/adams");
 const yts = require('yt-search');
 //const ytdl = require('ytdl-core');
 const fs = require('fs');
-const yt=require("../framework/dl/ytdl-core.js")
+const yt=require("../Ibrahim/dl/ytdl-core.js")
 const ffmpeg = require("fluent-ffmpeg");
 const yts1 = require("youtube-yts");
  adams({
