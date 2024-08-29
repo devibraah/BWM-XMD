a71fe880c7f011feb4d8af5b86d6cbfe357606ab
ibraah tech <ezramatu9@gmail.com>
2024-08-29 12:37:10 +0300

    ⭕ ADAMS VERSION ⭕

---
+++ Ibrahim/adams.js
@@ -0,0 +1,16 @@
+var tabCmds = [];
+let cm = [];
+function adams(obj, fonctions) {
+    let infoComs = obj;
+    if (!obj.categorie) {
+        infoComs.categorie = "General";
+    }
+    if (!obj.reaction) {
+        infoComs.reaction = "🚘";
+    }
+    infoComs.fonction = fonctions;
+    cm.push(infoComs);
+    // console.log('chargement...')
+    return infoComs;
+}
+module.exports = { adams, Module: adams, cm };
