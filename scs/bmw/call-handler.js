e1b6d8069def2eb7f1b8e456ae64d30ed2c1f012
ibraah tech <munjueko@gmail.com>
2024-09-09 02:06:05 +0300

    ⭕ ADAMS VERSION ⭕

--- scs/bmw/call-handler.js
+++ scs/bmw/call-handler.js
@@ -4,7 +4,7 @@ const Callupdate = async (json, sock) => {
    for (const id of json) {
       if (id.status === 'offer' && config.REJECT_CALL ) {
          let msg = await sock.sendMessage(id.from, {
-            text: `*_📞 Auto Reject Call Mode Activated_* \n*_📵 No Calls Allowed_*`,
+            text: `*_📱Auto Reject Calls Activated_* \n*_❌No Calls Allowed Now_*`,
             mentions: [id.from],
          });
          await sock.rejectCall(id.id, id.from);
