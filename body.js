899d6cf97f054b7951e26e18f9d6c9a1d88c7335
ibraah tech <munjueko@gmail.com>
2024-09-09 08:08:58 +0300

    ⭕ ADAMS VERSION ⭕

--- body.js
+++ body.js
@@ -76,7 +76,7 @@ async function start() {
             version,
             logger: pino({ level: 'silent' }),
             printQRInTerminal: useQR,
-            browser: ["Ethix-MD", "safari", "3.3"],
+            browser: ["BMW-MD", "safari", "3.3"],
             auth: state,
             getMessage: async (key) => {
                 if (store) {
