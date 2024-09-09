64fc2958edcba248411ab2369603afbdef709d29
ibraah tech <munjueko@gmail.com>
2024-09-09 08:25:35 +0300

    Update handler.js

--- scs/bmw/handler.js
+++ scs/bmw/handler.js
@@ -65,7 +65,7 @@ const Handler = async (chatUpdate, sock, logger) => {
         const { isGroup, type, sender, from, body } = m;
         console.log(m);
 
-        const pluginDir = path.join(__dirname, '..', 'plugin');
+        const pluginDir = path.join(__dirname, '..', 'bmworder');
         const pluginFiles = await fs.readdir(pluginDir);
 
         for (const file of pluginFiles) {
