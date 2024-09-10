226a83c84a2cef5df7801a921948a4fe753f0ae8
ibraah tech <munjueko@gmail.com>
2024-09-10 13:37:17 +0300

    Update bmwcmd.js

--- scs/bmworder/bmwcmd.js
+++ scs/bmworder/bmwcmd.js
@@ -21,18 +21,18 @@ const alive = async (m, Matrix) => {
 > *BMW MD COMMAND LIST*
 
  *This commands will guide*
- *you how to use me*
-
-  *BOT INFO*
-  
+ *you how to use me
+ 
+◤━━━━━━━━━━━◥  
+   *SYSTEM*
  ${prefix}𝙿𝚒𝚗𝚐
  ${prefix}𝙰𝚕𝚒𝚟𝚎
  ${prefix}𝙾𝚠𝚗𝚎𝚛
  ${prefix}𝙼𝚎𝚗𝚞
- ${prefix}𝙸𝚗𝚏𝚘𝚋𝚘𝚝
+◣━━━━━━━━━━━◢
 
-  *𝙾𝚆𝙽𝙴𝚁*
- 
+◤━━━━━━━━━━━◥
+   *𝙾𝚆𝙽𝙴𝚁*
 ${prefix}𝙹𝚘𝚒𝚗
 ${prefix}𝙻𝚎𝚊𝚟𝚎
 ${prefix}𝙱𝚕𝚘𝚌𝚔 
@@ -45,9 +45,10 @@ ${prefix}𝙰𝚞𝚝𝚘𝚃𝚢𝚙𝚒𝚗𝚐
 ${prefix}𝙰𝚕𝚠𝚊𝚢𝚜𝙾𝚗𝚕𝚒𝚗𝚎
 ${prefix}𝙰𝚞𝚝𝚘𝚁𝚎𝚊𝚍
 ${prefix}𝚊𝚞𝚝𝚘𝚜𝚟𝚒𝚎𝚠
+◣━━━━━━━━━━━◢
 
+◤━━━━━━━━━━━◥
    *GPT*
-  
  ${prefix}𝙰𝚒
  ${prefix}𝙱𝚞𝚐
  ${prefix}𝚁𝚎𝚙𝚘𝚛𝚝
@@ -55,9 +56,10 @@ ${prefix}𝚊𝚞𝚝𝚘𝚜𝚟𝚒𝚎𝚠
  ${prefix}𝙳𝚊𝚕𝚕𝚎
  ${prefix}𝚁𝚎𝚖𝚒𝚗𝚒
  ${prefix}𝙶𝚎𝚖𝚒𝚗𝚒
+◣━━━━━━━━━━━◢
  
+◤━━━━━━━━━━━◥
   *𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙴𝚁*
-  
  ${prefix}𝙰𝚃𝚃𝙿
  ${prefix}𝙰𝚃𝚃𝙿2
  ${prefix}𝙰𝚃𝚃𝙿3
@@ -65,17 +67,10 @@ ${prefix}𝚊𝚞𝚝𝚘𝚜𝚟𝚒𝚎𝚠
  ${prefix}𝙳𝙱𝙸𝙽𝙰𝚁𝚈
  ${prefix}𝙴𝙼𝙾𝙹𝙸𝙼𝙸𝚇
  ${prefix}𝙼𝙿3
+◣━━━━━━━━━━━◢
 
-  *𝚃𝙾𝙾𝙻*
-  
- ${prefix}𝙲𝚊𝚕𝚌𝚞𝚕𝚊𝚝𝚘𝚛
- ${prefix}𝚃𝚎𝚖𝚙𝚖𝚊𝚒𝚕
- ${prefix}𝙲𝚑𝚎𝚌𝚔𝚖𝚊𝚒𝚕
- ${prefix}𝚃𝚛𝚝
- ${prefix}𝚃𝚝𝚜
-
-  *𝙶𝚁𝙾𝚄𝙿* 
-  
+◤━━━━━━━━━━━◥
+   *𝙶𝚁𝙾𝚄𝙿* 
  ${prefix}𝙻𝚒𝚗𝚔𝙶𝚛𝚘𝚞𝚙
  ${prefix}𝚂𝚎𝚝𝚙𝚙𝚐𝚌
  ${prefix}𝚂𝚎𝚝𝚗𝚊𝚖𝚎
@@ -92,9 +87,10 @@ ${prefix}𝚊𝚞𝚝𝚘𝚜𝚟𝚒𝚎𝚠
  ${prefix}𝙿𝚛𝚘𝚖𝚘𝚝𝚎
  ${prefix}𝙳𝚎𝚖𝚘𝚝𝚎
  ${prefix}𝙶𝚎𝚝𝚋𝚒𝚘
+◣━━━━━━━━━━━◢
 
+◤━━━━━━━━━━━◥
  *𝙳𝙾𝚆𝙽𝙻𝙾𝙰𝙳*
- 
 ${prefix}𝙰𝚙𝚔
 ${prefix}𝙵𝚊𝚌𝚎𝚋𝚘𝚘𝚔
 ${prefix}𝙼𝚎𝚍𝚒𝚊𝚏𝚒𝚛𝚎
@@ -110,26 +106,21 @@ ${prefix}𝚅𝚒𝚍𝚎𝚘
 ${prefix}𝚈𝚝𝚖𝚙3𝚍𝚘𝚌
 ${prefix}𝚈𝚝𝚖𝚙4𝚍𝚘𝚌
 ${prefix}𝚃𝚒𝚔𝚝𝚘𝚔
+◣━━━━━━━━━━━◢
 
+◤━━━━━━━━━━━◥
  *𝚂𝙴𝙰𝚁𝙲𝙷*
- 
 ${prefix}𝙿𝚕𝚊𝚢
 ${prefix}𝚈𝚝𝚜
 ${prefix}𝙸𝚖𝚍𝚋
 ${prefix}𝙶𝚘𝚘𝚐𝚕𝚎
 ${prefix}𝙶𝚒𝚖𝚊𝚐𝚎
-${prefix}𝙿𝚒𝚗𝚝𝚎𝚛𝚎𝚜𝚝
-${prefix}𝚆𝚊𝚕𝚕𝚙𝚊𝚙𝚎𝚛
-${prefix}𝚆𝚒𝚔𝚒𝚖𝚎𝚍𝚒𝚊
-${prefix}𝚈𝚝𝚜𝚎𝚊𝚛𝚌𝚑
-${prefix}𝚁𝚒𝚗𝚐𝚝𝚘𝚗𝚎
 ${prefix}𝙻𝚢𝚛𝚒𝚌𝚜
+◣━━━━━━━━━━━◢
 
- *𝚂𝚃𝙰𝙻𝙺*
- 
-${prefix}𝚃𝚛𝚞𝚎𝚌𝚊𝚕𝚕𝚎𝚛
-${prefix}𝙸𝚗𝚜𝚝𝚊𝚂𝚝𝚊𝚕𝚔
-${prefix}𝙶𝚒𝚝𝚑𝚞𝚋𝚂𝚝𝚊𝚕𝚔
+*MORE COMMANDS*
+*WILL BE UPLOADED*
+*SOON*
 
 ▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄
 ▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄
